b9fec1e2072897f56f149de0154825e119e86664
Stefan NAE <stefan@lip.pt>
2018-07-06 04:23:26 +0100

    Fit events, in the third pass, with updated RAT.

--- process.sh
+++ process.sh
@@ -2,13 +2,17 @@
 
 # Initialization:
 
+RATVersion="6.5.2"
+
 # on Fermi
-source /lstore/sno/stefan/rat/load-rat-6.5.0.sh
+#/home/sno/stefan/
 #source ~/load-rat-water.sh
+#/lstore/sno/stefan/rat/
+source /lstore/sno/stefan/rat/load-rat-6.5.2.sh
 filePath="/lstore/sno/snoplus/Data/Water/Calibration/AmBe/"
 zdabFilePath=${filePath}"L2/"
-macFilePath=${filePath}"local/rat-6.5.0/raw_full/"
-outputFilePath=${filePath}"local/rat-6.5.0/raw_full/"
+macFilePath=${filePath}"local/rat-"${RATVersion}"/in_processing/"
+outputFilePath=${filePath}"local/rat-"${RATVersion}"/in_processing/"
 
 #source ~/load-rat-water.sh
 #zdabFilePath="/lstore/sno/stefan/data/backgrounds/"
@@ -34,32 +38,29 @@ do
     #echo ${zdabFilePath}${zdabFileName}.zdab
     if [ -f ${zdabFilePath}${zdabFileName}.zdab ]
     then
-        echo ${zdabFilePath}${zdabFileName}.zdab
+        echo "Found L2 file "${zdabFilePath}${zdabFileName}.zdab
 
 	# FIRST PASS
-	#nohup time rat -i ${zdabFilePath}${zdabFileName}.zdab ${macFilePath}first_pass_data_cleaning.mac > ${outputFilePath}SNOP_0000109133_001.l2.t_800_n1_16_n2_4.first_pass.log &
-	time rat -i ${zdabFilePath}${zdabFileName}.zdab ${macFilePath}first_pass_data_cleaning.mac > ${outputFilePath}${zdabFileName}.first_pass.log
+	time rat -n $r -i ${zdabFilePath}${zdabFileName}.zdab ${macFilePath}first_pass_data_cleaning.mac > ${outputFilePath}${zdabFileName}.first_pass.log
         #continue
 
 	# SECOND PASS
-	if [ -f ${outputFilePath}${zdabFileName}.root ]
+	if [ -f ${outputFilePath}"second_pass/"${zdabFileName}.root ]
 	then
 	    echo "A second pass root file already exists."
 	else
-	    #nohup time rat -i ${zdabFilePath}${zdabFileName}.zdab -o ${outputFilePath}{zdabFileName}.root ${macFilePath}second_pass_processing.mac > ${outputFilePath}${zdabFileName}.second_pass.log &
-	    time rat -i ${zdabFilePath}${zdabFileName}.zdab -o ${outputFilePath}${zdabFileName}.root ${macFilePath}second_pass_processing.mac > ${outputFilePath}${zdabFileName}.second_pass.log
+	    time rat -n $r -i ${zdabFilePath}${zdabFileName}.zdab -o ${outputFilePath}"second_pass/"${zdabFileName}.root ${macFilePath}second_pass_processing.mac > ${outputFilePath}${zdabFileName}.second_pass.log
 	fi
 
 	# THIRD PASS
-	#processedFileName="SNOP_0000"$r"_00"$subfile"_l2"
-	#if [ -f ${outputFilePath}${processedFileName}_third_pass.root ]
-	#then
-	#    echo "A third pass root file already exists."
-	#else
-           # nohup time rat -i ${outputFilePath}${zdabFileName}.root -o ${outputFilePath}${processedFileName} ${macFilePath}third_pass_analysis_processing_AmBe5-testing.mac > ${outputFilePath}${zdabFileName}.third_pass.log &
-           #time rat -i ${outputFilePath}${zdabFileName}.root -o ${outputFilePath}${processedFileName} ${macFilePath}third_pass_analysis_processing_AmBe5-testing2.mac > ${outputFilePath}${zdabFileName}.third_pass.log
-        #   time rat -i ${outputFilePath}${zdabFileName}.root -o ${outputFilePath}${processedFileName} ${macFilePath}third_pass_AmBe_processing.mac > ${outputFilePath}${zdabFileName}.third_pass.log
-	#fi
+	processedFileName="SNOP_0000"$r"_00"$subfile"_l2"
+	if [ -f ${outputFilePath}"third_pass/"${processedFileName}.root ]
+	then
+	    echo "A third pass root file already exists."
+	else
+	    echo "Adding spices."
+            time rat -n $r -i ${outputFilePath}"second_pass/"${zdabFileName}.root -o ${outputFilePath}"third_pass/"${processedFileName} ${macFilePath}third_pass_AmBe_processing.mac > ${outputFilePath}${zdabFileName}.third_pass.log
+	fi
     fi
   done
 done
